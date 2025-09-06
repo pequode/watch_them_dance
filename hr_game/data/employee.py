509e2ace765eca91d7876a627e3b7872dc0c237c
pequode <your_email@example.com>
2025-09-06 10:42:27 -0400

    feat: added update method

--- hr_game/data/employee.py
+++ hr_game/data/employee.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel, Field
-
+def bound(inp,top,bottom):
+  return max(bottom,min(top,inp))
 class Trait(BaseModel):
   """Traits modify deltas. The name is the field that its referring to. The effect is how it gets modified"""
   name:str
   effect: float 
 
+class EmployeeDelta(BaseModel):
+  stress: int = Field(...,help="The change in stress of the employee.")
+  greed: int = Field(...,help="How much this employee's motivation for money has changed (on a scale from -100-100).")
+  salary: int = Field(...,help="How much this employee's salary has changed")
+  anger: int = Field(...,help="How much angrier the employee is on a scale (from -100-100).")
+  happiness:int = Field(...,help="How the happiness of this employee has changed on a scale (from -100-100).")
+  health:int = Field(...,help="How the health of this employee has changed on a scale (from -100-100).")
 class Employee(BaseModel):
   name:str
   age: int = Field(...,help="The age of the employee")
@@ -15,13 +23,19 @@ class Employee(BaseModel):
   horniness: int = Field(0,help="The lobito of this employee on a scale from 0-100")
   happiness:int = Field(50,help="The happiness of this employee on a scale from 0-100")
   productivity:int = Field(10,help="How productive this employee is on a scale from 0-100")
+  health: int =Field(75,help="How healthy this employee is on a scale from 0-100")
+  context_history:list[str] = Field(default_factory=lambda x: [],help="A list of traits that that employee has.") 
   traits: list[Trait] = Field(default_factory=lambda x: [],help="A list of traits that that employee has.")
-class EmployeeDelta(BaseModel):
-  stress: int = Field(...,help="The change in stress of the employee.")
-  greed: int = Field(...,help="How much this employee's motivation for money has changed (on a scale from 0-100).")
-  salary: int = Field(...,help="How much this employee's salary has changed")
-  anger: int = Field(...,help="How much angrier the employee is on a scale (from 0-100).")
-  happiness:int = Field(...,help="The happiness of this employee has changed on a scale (from 0-100).")
+  def update(self,other:EmployeeDelta):
+    self.stress = bound(other.stress+self.stress,100,0)
+    self.greed = bound(other.greed+self.greed,100,0)
+    self.salary = bound(other.greed+self.greed,100,0)
+    self.anger = bound(other.anger + self.anger,100,0)
+    self.health = bound(other.health + self.health,100,0)
+    self.happiness = bound(other.happiness + self.happiness,100,0)
+
+
+
 
 class EmployeeRelationship(BaseModel):
   attraction: float =Field(1.0,help="The attraction multiplier between these two employees.")
